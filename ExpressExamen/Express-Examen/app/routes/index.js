92fc66b4b7b0510c8025223e0916c6623c764100
VincentHertens <55692053+VincentHertens@users.noreply.github.com>
2020-01-20 10:34:04 +0100

    Studenten verkrijgen
    Connectie met mongodb,  GET functie gemaakt voor de studenten

--- ExpressExamen/Express-Examen/app/routes/index.js
+++ ExpressExamen/Express-Examen/app/routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
+/* mongodb connectie */
+var MongoClient = require('mongodb').MongoClient;
+var db;
+MongoClient.connect('mongodb://localhost:27017', (err, database) => {
+    if(err) return console.log(err);
+    db = database.db('exam');
+})
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.send("Hello World");
-});
+router.get('/', (req, res) => {
+  db.collection('students').find().sort({naam:1}).toArray((err, result) => {
+      if(err) return console.log(err);
+      res.render('students.ejs', {studenten: result});
+  })
+})
 
 module.exports = router;
